a76bcbaaa060b7e1943ca21174f4c1867e4bf338
AthenaXu <mingxia1510@163.com>
2017-07-15 10:49:21 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Athena Xu on 2017/6/22.
+ */
+var index2=require('./index2.js');
+require("!style-loader!css-loader!./style.css");
+document.write('It works!');
+document.write(index2);
---
+++ index2.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Athena Xu on 2017/6/22.
+ */
+module.exports="It works from index2.js";
